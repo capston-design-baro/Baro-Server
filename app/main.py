085335529ff5b6a8980368002c240c4b12bb0bda
김나영 <kny2383@gmail.com>
2025-12-05 13:25:37 +0900

    [FEAT] 배포된 프론트 URL allow_origins 설정

--- app/main.py
+++ app/main.py
@@ -29,7 +29,9 @@ app = FastAPI(
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", 
+    "https://baro-front-five.vercel.app",
+    "https://baro-front-qlq4xlmhw-soyeons-projects-0593c180.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
